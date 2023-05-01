6a39330c8a78d1e86d0db72c10cb12a604c01751
Towhid-Raiyan <towhid.raiyan.com>
2023-05-02 04:45:40 +0600

    footer added

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -4,7 +4,13 @@ import { Container } from 'react-bootstrap';
 const Footer = () => {
     return (
         <div className='bg-warning mb-0  opacity-75'>
-            
+            <Container className='d-flex justify-content-between py-4 text-white'>
+                <h4><i>Indian Taste</i></h4>
+                <h4>contact us</h4>
+                <h4 className=''>
+                    <small>© 2023 Indian Taste. All Rights Reserved.</small>
+                </h4>
+            </Container>
         </div>
     );
 };
