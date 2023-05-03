06111a9a6e96cdc1c43b13b7c2bc6a97146bc786
Towhid-Raiyan <towhid.raiyan.com>
2023-05-04 03:26:51 +0600

    login  page  added

--- src/pages/login/Login/Login.jsx
+++ src/pages/login/Login/Login.jsx
@@ -4,7 +4,36 @@ import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div></div>
+        <Container className='my-5 py-5 w-25'>
+            <h2 className='fs-1 fw-bolder'>Please Login !</h2>
+            <Form className='my-5 bg-warning-subtle'>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name='email' placeholder="Enter email" required/>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name='password' placeholder="Password" required/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="warning w-75 text-white fw-bold" type="submit">
+                    Login
+                </Button>
+                <br />
+                <Form.Text className="text-secondary fw-bold">
+                    Don't have an account ? <Link to='/register'>Register</Link>
+                </Form.Text>
+                <Form.Text className="text-success">
+                    We'll never share your email with anyone else.
+                </Form.Text>
+                <Form.Text className="text-danger">
+                    We'll never share your email with anyone else.
+                </Form.Text>
+            </Form>
+        </Container>
     );
 };
 
